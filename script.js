96fa82f562598a0830e85b79d19afea65eccaac5
oussamakami <kamili.oussama1@gmail.com>
2020-12-26 11:59:11 +0100

    add copyToClipboard to outputDisplay elem

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const length = selectItem("lengthRange"),
   lengthPrev = selectItem("lengthPreview"),
   generateBtn = selectItem("generateBtn"),
   passwdOutput = selectItem("passwdOutput"),
+  outputDisplay = selectItem("outputDisplay"),
   settingSelectors = document.querySelectorAll("[type='checkbox']");
 
 window.addEventListener("load", () => {
   length.addEventListener("input", updateLengthPrev);
   updateLengthPrev();
+  outputDisplay.addEventListener("click", copyToClipboard);
 });
 
 function selectItem(id) {
@@ -15,4 +17,16 @@ function selectItem(id) {
 
 function updateLengthPrev() {
   lengthPrev.innerHTML = length.value;
+}
+
+function copyToClipboard() {
+  const elem = document.createElement("textarea"),
+    txtValue = passwdOutput.innerText;
+
+  elem.style.cssText = "position: absolute; left:-9999px";
+  elem.value = txtValue;
+  document.body.appendChild(elem);
+  elem.select();
+  document.execCommand("copy");
+  document.body.removeChild(elem);
 }
