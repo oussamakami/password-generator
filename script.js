0ef310ec17c493288233667afa3606268bc2331a
oussamakami <kamili.oussama1@gmail.com>
2020-12-27 09:50:00 +0100

    keep at least one setting checked

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ window.addEventListener("load", () => {
   length.addEventListener("input", updateLengthPrev);
   updateLengthPrev();
   outputDisplay.addEventListener("click", copyToClipboard);
+  settingSelectors.forEach((elem) => {
+    elem.addEventListener("input", keepOneSettingChecked);
+  });
 });
 
 function selectItem(id) {
@@ -29,4 +32,17 @@ function copyToClipboard() {
   elem.select();
   document.execCommand("copy");
   document.body.removeChild(elem);
+}
+
+function keepOneSettingChecked() {
+  let checkedSettings = document.querySelectorAll(
+      "input[type=checkbox]:checked"),
+    disabledSettings = document.querySelectorAll(
+      "input[type=checkbox]:disabled");
+
+  if (checkedSettings.length < 2) {
+    checkedSettings[0].disabled = 1;
+  } else if (checkedSettings.length > 1 && disabledSettings.length > 0) {
+    disabledSettings[0].disabled = 0;
+  }
 }
